d1ebf7f75fa05b9472ddda650b49d5d94e7f1821
Suraaj Hasija <mailbox.suraaj@gmail.com>
2024-03-07 02:30:17 +0530

    v3

--- test_hello.py
+++ test_hello.py
@@ -15,7 +15,7 @@ def test_predictions(client):
     test_data= {
     "Gender": "Male",
     "Married": "Yes",
-    "ApplicantIncome": 5000000,
+    "ApplicantIncome": 5500000,
     "LoanAmount": 5000,
     "Credit_History": 1.0}
 
